cb4a75f3e4d86d38904f56088ca6115922367e2b
leleluv1122 <jh_o214@naver.com>
2020-04-26 01:17:57 +0900

    plus 실습과제2

---
+++ practice/src/three/Hw3.java
@@ -0,0 +1,55 @@
+package three;
+
+public class Hw3 {
+	static Object[][] orr = new Object[6][2];
+
+	static void addData(int count) {
+		for (int i = 0; i < count; i++) {
+			// orr[i] = new Object[2];
+
+			orr[i][0] = new Integer(i);
+			orr[i][1] = String.valueOf(i);
+		}
+	}
+
+	static int findIndex(int value) {
+		int idx = 0;
+		for (int i = 0; i < orr.length; i++) {
+			if (orr[i][0].equals(value))
+				idx = i;
+		}
+		return idx;
+	}
+	
+	static int findIndex(String value) {
+		int idx = 0;
+		for (int i = 0; i < orr.length; i++) {
+			if (orr[i][1].equals(value))
+				idx = i;
+		}
+		return idx;
+	}
+
+	public static void main(String[] args) {
+		addData(6);
+		System.out.println(findIndex(3));
+		System.out.println(findIndex("3"));
+	}
+}
+
+/*
+ * public class Hw3 { static Object[][] orr = new Object[3][2];
+ * 
+ * static void addData(int count) { for (int i = 0; i < count; i++) { //orr[i] =
+ * new Object[2];
+ * 
+ * orr[i][0] = new Integer(i); orr[i][1] = String.valueOf(i); } }
+ * 
+ * static int findIndex(int value) { for(int i=0;i<3;i++) {
+ * 
+ * } }
+ * 
+ * public static void main(String[] args) {
+ * 
+ * } }
+ */
